49dc770364e837209e7f07ceeb2503b6289d31ee
Md-Zehad-Sarkar <zehadsarkar22@outlook.com>
2024-03-20 22:44:59 +0600

    sunbeam-kids-garden-server project setup and initialized

---
+++ README.md
@@ -0,0 +1,24 @@
+# Project Name
+
+- sunbeam-kids-garden-server
+
+# Technologies
+
+- typescript
+- node/express
+- mongoose
+-  dotenv, eslint
+
+# Implements 
+- user create
+- all users find
+- user find by userId
+- update user information
+- delete a user
+- mongoose schema, model, middleware
+- password hashing with bcrypt
+- user validation with zod
+- eslint and prettier
+
+# Deploy 
+- vercel
---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import { userRoutes } from './app/modules/users/user.route';
+const app: Application = express();
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+//application routes
+app.use('/', userRoutes);
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('user data and order management server');
+});
+
+export default app;
